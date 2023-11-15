3e6700e7febdef60744aa595c0e04f272b6f8384
tyb <1474326442@qq.com>
2023-11-15 13:54:12 +0800

    完善密码MD5加密

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+}
---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.sky.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+}
